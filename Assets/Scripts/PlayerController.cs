ee0a73bd109080bc5ca7220a604391f397fc3dba
Sean Lynch <seanzolynch@gmail.com>
2023-06-23 12:34:56 -0600

    Revert "Merge branch 'shaylyn_version' into main"
    This reverts commit 2442cb3920c0b727a83182c8acdb44651df3d441.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-<<<<<<< Updated upstream
     int myStupidVariable = 12;
-=======
-    int myStupidVariable = 11;
->>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
